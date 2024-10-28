c40ebca4cd4d95e411101edc61aba94f42fbe9c5
rathoreaakansha <156983484+rathoreaakansha@users.noreply.github.com>
2024-10-28 08:47:10 +0000

    datatype and variables

---
+++ Datatype.js
@@ -0,0 +1,15 @@
+let state="jaipur"
+let age=25
+let salary=213.50
+
+//number
+//string
+//bigint
+//undefined
+//null
+//boolean
+
+console.log(typeof"jaipur");
+console.log(typeof 231.50)
+console.log(typeof null)     //type of null is object
+console.log(typeof undefined)
---
+++ Variable.js
@@ -0,0 +1,9 @@
+const account_id=144214;
+let accountName = "Aakansha";
+var account_email="rathoreAakansha29@gmail.com";
+ let account_city="indore"
+ let accountstate;
+
+console.table([account_id,accountName,account_email,account_city,accountstate])
+console.log(account_id);
+
